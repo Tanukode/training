9957848620bee447a91c271d4761e89af8fa1dcf
Tanukode <nicolas.huerta@gmail.com>
2024-03-19 14:37:25 -0300

    corrected UserService

--- src/main/java/com/tanukode/training/service/UserService.java
+++ src/main/java/com/tanukode/training/service/UserService.java
@@ -23,7 +23,6 @@ public class UserService {
     @Autowired
     PasswordEncoder encoder;
 
-
     public Flux<User> getUsers() {
         return repository.findAll();
     }
@@ -34,27 +33,32 @@ public class UserService {
 
     public Mono<User> saveUser(User user) {
         return repository.findByUsername(user.getUsername()).map(Optional::of)
-        .defaultIfEmpty(Optional.empty())
-        .flatMap(optionalUser->{
-            if(optionalUser.isPresent()){
-                return Mono.error(new Exception("Username already exists"));
-            }else{
-                User newUser = new User(null, user.getUsername(), encoder.encode(user.getPassword()), user.getEmail()
-                , user.getFirstName(), user.getLastName());
-                return repository.save(newUser);
-            }
-        });
+                .defaultIfEmpty(Optional.empty())
+                .flatMap(optionalUser -> {
+                    if (optionalUser.isPresent()) {
+                        return Mono.error(new Exception("Username already exists"));
+                    } else {
+                        User newUser = new User(null, user.getUsername(), encoder.encode(user.getPassword()),
+                                user.getEmail(), user.getFirstName(), user.getLastName());
+                        return repository.save(newUser);
+                    }
+                });
     }
 
-    public Mono<User> updateUser(User user){
+    public Mono<User> updateUser(User user) {
         UUID uid = user.getUid();
-        return repository.findById(uid).flatMap(
-            existingUser ->{
-                User newUser = new User(uid, user.getUsername(), encoder.encode(user.getPassword()), user.getEmail()
-                , user.getFirstName(), user.getLastName());
-                return repository.save(newUser);
-            }
-        ).switchIfEmpty(Mono.error(new Exception("User not found")));
+        return repository.findById(uid).map(Optional::of).defaultIfEmpty(Optional.empty()).flatMap(
+                optionalUser -> {
+                    String newUserName = user.getUsername();
+                    String currentUserName = optionalUser.get().getUsername();
+                    if (optionalUser.isEmpty()) {
+                        return Mono.error(new Exception("User not found"));
+                    }
+                    if (newUserName.equals(currentUserName)){
+                        return repository.save(user);
+                    }
+                    return repository.findByUsername(newUserName).switchIfEmpty(repository.save(user));
+                });
     }
 
     public Mono<String> deleteUser(UUID uid) {
