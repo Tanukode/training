5937e40cd7c2e3c0e1429a6a83c79e75aa82b76b
Tanukode <nicolas.huerta@gmail.com>
2024-03-21 12:41:02 -0300

    example method authorization added

--- src/main/java/com/tanukode/training/config/SecurityConfig.java
+++ src/main/java/com/tanukode/training/config/SecurityConfig.java
@@ -3,10 +3,14 @@ package com.tanukode.training.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
+@EnableWebFluxSecurity
+@EnableReactiveMethodSecurity
 public class SecurityConfig {
 
     @Bean
@@ -18,7 +22,6 @@ public class SecurityConfig {
                 .anyExchange().authenticated())
                 .csrf(csrf->csrf.disable())
                 .formLogin(Customizer.withDefaults());
-
         return http.build();
     }
 
--- src/main/java/com/tanukode/training/controller/handler/UserHandler.java
+++ src/main/java/com/tanukode/training/controller/handler/UserHandler.java
@@ -5,6 +5,7 @@ import static org.springframework.web.reactive.function.server.ServerResponse.*;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -53,6 +54,7 @@ public class UserHandler {
         return ok().body(userMono, User.class);
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public Mono<ServerResponse> delete(ServerRequest request){
         UUID uid = UUID.fromString(request.pathVariable("uid"));
         return ok().body(userService.deleteUser(uid), User.class);
